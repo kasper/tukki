5503f83460a65b56521368b1e33a8719453f95fc
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2012-03-20 00:21:18 +0200

    Fix Heroku MongoDB password to be a string

--- src/main/java/wad/tukki/controller/RootController.java
+++ src/main/java/wad/tukki/controller/RootController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class RootController {
 
-    @RequestMapping("*")
-    public String root() {
+    @RequestMapping("/")
+    public String root() {       
         return "static/index.html";
     }
 }
