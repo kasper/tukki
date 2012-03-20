ad3ce212f7f412cd42e24f3672aa31f8c6b3053a
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2012-03-20 15:13:03 +0200

    Remove old files

--- src/main/java/wad/tukki/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package wad.tukki.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import wad.tukki.model.Product;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-
-}
--- src/main/java/wad/tukki/service/ProductService.java
+++
@@ -1,8 +0,0 @@
-package wad.tukki.service;
-
-import wad.tukki.model.Product;
-
-public interface ProductService {
-    public Product addProduct(Product product);
-    public Product findById(String id);
-}
