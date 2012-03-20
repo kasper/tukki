eb3f741b1aea753ecada80d329bac8daaf7b1eaa
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2012-03-20 19:07:18 +0200

    Backbone routers

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -1,37 +1,86 @@
 var tukki = {
+
   models: {},
   collections: {},
-  views: {}
+  views: {},
+  routers: {},
+  
+  init: function() {
+  
+    new tukki.routers.Main();
+    Backbone.history.start();
+  }
+  
 }
 
 /* Models */
+ 
 tukki.models.Product = Backbone.Model.extend({
 
   urlRoot : 'api/product'
+  
 });
 
 /* Collections */
+
 tukki.collections.Products = Backbone.Model.extend({
   
-  model: tukki.models.Product,
+  model: tukki.models.Product, 
   url: 'api/products'
+  
 });
 
 /* Views */
+
 tukki.views.ProductView = Backbone.View.extend({
 
   render: function() {
+  
     var template = $('#product-template').html();
     var output = Mustache.render(template, this.model.toJSON());
-    $('body').html(output);
+    $('#content').html(output);
   }
+  
+});
+
+/* Routers */
+
+tukki.routers.Main = Backbone.Router.extend({
+
+  routes: {
+  
+    '':             'index',
+    '/':            'index',
+    '/products':    'products',
+    '/product/:id': 'product'
+    
+  },
+  
+  index: function() {
+  
+    console.log('Index');
+  },
+  
+  products: function() {
+  
+    console.log('List products');
+  },
+  
+  product: function(id) {
+  
+    console.log('Show product ' + id);
+  }
+  
 });
 
 $(document).ready(function() {
 
-  var product = new tukki.models.Product({id: "4f6877940364a650f6c8930c", name: "Hello World!"});
+  tukki.init();
+
+  var product = new tukki.models.Product({id: '4f6877940364a650f6c8930c', name: 'Hello World!'});
   var productView = new tukki.views.ProductView({model: product});
   product.on('change', function() { productView.render() });
   //product.fetch();
   productView.render();
+  
 });
